c52c6a0c3fddcdcc4fbb54eed7dcb7c5659738e8
Nirmal2796 <nirmalgadekar2796@gmail.com>
2024-08-03 19:25:50 +0530

    LeaderBoard

--- leaderboard/main.js
+++ leaderboard/main.js
@@ -1,36 +1,56 @@
+
+//token
+
+document.addEventListener('DOMContentLoaded',showLeaderBoard);
+
+const leaderboard_table = document.getElementById("leaderboard-table-body");
+
+
 //SHOW LEADERBOARD
-async function showLeaderBoard(){
+async function showLeaderBoard() {
 
-    const token=localStorage.getItem('token');
+    const token = localStorage.getItem('token');
+    console.log(token);
+    const result = await axios.get("http://localhost:3000/showleaderboard", { headers: { "Auth": token } })
 
-    const result=await axios.get("http://localhost:3000/premium/showleaderboard",{headers:{"Authorization":token}})
+    console.log(result.data);
 
-    // console.log(result.data.result);
-
-    LeaderBoardListDiv.hidden=false;
     
-    leaderboard_list.innerHTML='';
+
+    for (let i in result.data) {
+
+        let newRow;
+
+        if (result.data[i].total_cost == null) {
 
 
-    for(let i in result.data.result){
-  
-        let child;
+            newRow = `<tr id=${result.data[i].id+1}  
+             class="list-group-item odd:bg-white even:bg-[#799e9b] text-[#154e49] font-semibold  border-b"> 
+              <td class="px-6 py-3">
+                    ${result.data[i].userId} </td> 
+                    <td class="px-6 py-3">
+                    ${result.data[i].name} </td> 
+                    <td class="px-6 py-4">0</td> 
+                    </tr>`;
 
-        if(result.data.result[i].total_expense == null){
-            child=`<li id=${result.data.result[i].id} class="list-group-item"> 
-                    ${result.data.result[i].uname} - 0 
-                    </li>`
+
         }
-        else{
-            // console.log(result.data.result[i].user);
-            child=`<li id=${result.data.result[i].id} class="list-group-item"> 
-                ${result.data.result[i].uname} - ${result.data.result[i].total_expense}  
-                </li>`
+        else {
+            newRow = `<tr id=${result.data[i].id}  
+                class="list-group-item odd:bg-white even:bg-[#799e9b] text-[#154e49] font-semibold  border-b"> 
+                 <td class="px-6 py-3">
+                       ${result.data[i].userId} </td> 
+                       <td class="px-6 py-3">
+                       ${result.data[i].name} </td> 
+                       <td class="px-6 py-4">${result.data[i].total_cost} </td> 
+                       </tr>`;
+
         }
 
-            leaderboard_list.innerHTML=leaderboard_list.innerHTML+child;
+        leaderboard_table.insertAdjacentHTML('afterend', newRow);
     }
 
-    LeaderBoardListDiv.scrollIntoView();
-    
+
 }
+
+
